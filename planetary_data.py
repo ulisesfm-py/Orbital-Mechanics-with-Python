1e6b86e74aa2a1568d4a1288f511f383ca0f1113
ulisesfm-py <ulisesfm.py@gmail.com>
2024-10-31 09:14:13 +0100

    Adding first lessons

---
+++ planetary_data.py
@@ -0,0 +1,13 @@
+sun = {
+    'name': 'Sun',
+    'mass': 1.989e30,
+    'mu': 1.32712e11,
+    'radius': 695700.0,
+}
+
+earth = {
+    'name': 'Earth',
+    'mass': 5.972e24,
+    'mu': 5.972e24*3.986e14,
+    'radius': 6378.0,
+}
